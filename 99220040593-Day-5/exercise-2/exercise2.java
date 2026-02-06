0ea8093dff8877eaf755ce5f6fdf9a706898364d
bala-rajesh <kunapareddybalarajesh@gmail.com>
2026-02-06 10:31:47 +0530

    Adding Day-5 upto exercise-3

---
+++ 99220040593-Day-5/exercise-2/exercise2.java
@@ -0,0 +1,10 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        Zerop zerop = () -> System.out.println("2-82,Avanigadda,Krishna District,Andhra Pradesh,521401 ");
+        zerop.display();
+    }
+}
+
+interface Zerop {
+    void display();
+}
